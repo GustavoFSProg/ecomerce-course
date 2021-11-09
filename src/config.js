f832f60d38798fcb5adc260cb74dc07c7c6b4fae
Gustavo <gustavo.prog40@gmail.com>
2021-11-09 11:17:07 -0300

    fff

--- src/config.js
+++ src/config.js
@@ -4,7 +4,9 @@ dotenv.config()
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
+  MONGODB_URL:
+    process.env.MONGODB_URL ||
+    'mongodb+srv://gustavo:TezzCP3ByRzs1Kw4@cluster0.1ehab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   accessKeyId: process.env.accessKeyId || 'accessKeyId',
