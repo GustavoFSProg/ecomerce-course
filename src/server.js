e697604b8d9221115158588d8921942dc2e6134c
Gustavo <gustavo.prog40@gmail.com>
2021-11-09 12:48:01 -0300

    tentantdo

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path'
 import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import config from './config'
@@ -26,19 +27,21 @@ mongoose
   .catch((error) => console.log(error.reason))
 
 const app = express()
+app.use(bodyParser.json())
 app.use('/api/uploads', uploadRoute)
 app.use('/api/users', userRoute)
 app.use('/api/products', productRoute)
 app.use('/api/orders', orderRoute)
+app.use('/uploads', express.static(path.join(__dirname, '/../uploads')))
 app.use(route) // app.get('/api/config/paypal', (req, res) => {
 //   res.send(config.PAYPAL_CLIENT_ID)
 // })
-app.use('/uploads', express.static(path.join(__dirname, '/../uploads')))
-app.use(express.static(path.join(__dirname, '/../frontend/build')))
+// app.use(express.static(path.join(__dirname, '/../frontend/build')))
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(`${__dirname}/../backend/template/index.html`))
 // })
 app.use(cors())
+app.use(route)
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`)
